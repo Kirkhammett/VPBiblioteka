5f15a5eb45928bdc16572f4c97d085cb78afc110
Filip <boev.filip@students.finki.ukim.mk>
2015-04-27 01:11:06 +0200

    Bulk changes
    A couple of changes to the overall look of the WF, and a few
    functionalities ralating to the database.

--- source/VPBiblioteka/Form1.Designer.cs
+++ source/VPBiblioteka/Form1.Designer.cs
@@ -28,151 +28,271 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lbKnigi = new System.Windows.Forms.ListBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbSurname = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPubHouse = new System.Windows.Forms.TextBox();
+            this.tbPubYear = new System.Windows.Forms.TextBox();
+            this.rtbSummary = new System.Windows.Forms.RichTextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.btnDodadi = new System.Windows.Forms.Button();
+            this.btnIzbrisi = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
+            this.cbHorror = new System.Windows.Forms.CheckBox();
+            this.cbMystery = new System.Windows.Forms.CheckBox();
+            this.cbThriller = new System.Windows.Forms.CheckBox();
+            this.cbFantasy = new System.Windows.Forms.CheckBox();
+            this.cbDrama = new System.Windows.Forms.CheckBox();
+            this.cbRomance = new System.Windows.Forms.CheckBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // listBox1
+            // lbKnigi
             // 
-            this.listBox1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 25;
-            this.listBox1.Location = new System.Drawing.Point(37, 75);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(421, 554);
-            this.listBox1.TabIndex = 0;
+            this.lbKnigi.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.lbKnigi.FormattingEnabled = true;
+            this.lbKnigi.Location = new System.Drawing.Point(18, 39);
+            this.lbKnigi.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lbKnigi.Name = "lbKnigi";
+            this.lbKnigi.Size = new System.Drawing.Size(262, 290);
+            this.lbKnigi.TabIndex = 0;
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.tbSurname);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.textBox3);
-            this.panel1.Controls.Add(this.textBox2);
-            this.panel1.Controls.Add(this.richTextBox1);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Location = new System.Drawing.Point(593, 44);
+            this.panel1.Controls.Add(this.tbPubHouse);
+            this.panel1.Controls.Add(this.tbPubYear);
+            this.panel1.Controls.Add(this.rtbSummary);
+            this.panel1.Controls.Add(this.tbName);
+            this.panel1.Location = new System.Drawing.Point(296, 23);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(567, 778);
+            this.panel1.Size = new System.Drawing.Size(284, 405);
             this.panel1.TabIndex = 1;
             // 
-            // textBox1
+            // label6
             // 
-            this.textBox1.Location = new System.Drawing.Point(50, 47);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(281, 31);
-            this.textBox1.TabIndex = 0;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 42);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(111, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Презиме на авторот";
             // 
-            // richTextBox1
+            // tbSurname
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(50, 309);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(498, 430);
-            this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(37, 668);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(178, 50);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Додади";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(50, 129);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(281, 31);
-            this.textBox2.TabIndex = 2;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(50, 211);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(281, 31);
-            this.textBox3.TabIndex = 3;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(45, 19);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(279, 25);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Име и презиме на авторот";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(45, 101);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(219, 25);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Година на издавање";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(45, 183);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(170, 25);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Издавачка куќа";
+            this.tbSurname.Location = new System.Drawing.Point(25, 57);
+            this.tbSurname.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbSurname.Name = "tbSurname";
+            this.tbSurname.ReadOnly = true;
+            this.tbSurname.Size = new System.Drawing.Size(142, 20);
+            this.tbSurname.TabIndex = 8;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(45, 265);
+            this.label4.Location = new System.Drawing.Point(22, 144);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(187, 25);
+            this.label4.Size = new System.Drawing.Size(96, 13);
             this.label4.TabIndex = 7;
             this.label4.Text = "Кратка содржина";
             // 
-            // button2
+            // label3
             // 
-            this.button2.Location = new System.Drawing.Point(280, 668);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(178, 50);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Избриши";
-            this.button2.UseVisualStyleBackColor = true;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 107);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Издавачка куќа";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 74);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Година на издавање";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Име на авторот";
+            // 
+            // tbPubHouse
+            // 
+            this.tbPubHouse.Location = new System.Drawing.Point(25, 121);
+            this.tbPubHouse.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbPubHouse.Name = "tbPubHouse";
+            this.tbPubHouse.ReadOnly = true;
+            this.tbPubHouse.Size = new System.Drawing.Size(142, 20);
+            this.tbPubHouse.TabIndex = 3;
+            // 
+            // tbPubYear
+            // 
+            this.tbPubYear.Location = new System.Drawing.Point(25, 89);
+            this.tbPubYear.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbPubYear.Name = "tbPubYear";
+            this.tbPubYear.ReadOnly = true;
+            this.tbPubYear.Size = new System.Drawing.Size(142, 20);
+            this.tbPubYear.TabIndex = 2;
+            // 
+            // rtbSummary
+            // 
+            this.rtbSummary.Location = new System.Drawing.Point(25, 167);
+            this.rtbSummary.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rtbSummary.Name = "rtbSummary";
+            this.rtbSummary.ReadOnly = true;
+            this.rtbSummary.Size = new System.Drawing.Size(251, 219);
+            this.rtbSummary.TabIndex = 1;
+            this.rtbSummary.Text = "";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(25, 24);
+            this.tbName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbName.Name = "tbName";
+            this.tbName.ReadOnly = true;
+            this.tbName.Size = new System.Drawing.Size(142, 20);
+            this.tbName.TabIndex = 0;
+            // 
+            // btnDodadi
+            // 
+            this.btnDodadi.Location = new System.Drawing.Point(18, 394);
+            this.btnDodadi.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDodadi.Name = "btnDodadi";
+            this.btnDodadi.Size = new System.Drawing.Size(89, 26);
+            this.btnDodadi.TabIndex = 2;
+            this.btnDodadi.Text = "Додади";
+            this.btnDodadi.UseVisualStyleBackColor = true;
+            // 
+            // btnIzbrisi
+            // 
+            this.btnIzbrisi.Enabled = false;
+            this.btnIzbrisi.Location = new System.Drawing.Point(190, 394);
+            this.btnIzbrisi.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnIzbrisi.Name = "btnIzbrisi";
+            this.btnIzbrisi.Size = new System.Drawing.Size(89, 26);
+            this.btnIzbrisi.TabIndex = 3;
+            this.btnIzbrisi.Text = "Избриши";
+            this.btnIzbrisi.UseVisualStyleBackColor = true;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(32, 44);
+            this.label5.Location = new System.Drawing.Point(16, 24);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(70, 25);
+            this.label5.Size = new System.Drawing.Size(37, 13);
             this.label5.TabIndex = 4;
             this.label5.Text = "Книги";
             // 
+            // cbHorror
+            // 
+            this.cbHorror.AutoSize = true;
+            this.cbHorror.Location = new System.Drawing.Point(18, 337);
+            this.cbHorror.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbHorror.Name = "cbHorror";
+            this.cbHorror.Size = new System.Drawing.Size(57, 17);
+            this.cbHorror.TabIndex = 5;
+            this.cbHorror.Text = "Хорор";
+            this.cbHorror.UseVisualStyleBackColor = true;
+            // 
+            // cbMystery
+            // 
+            this.cbMystery.AutoSize = true;
+            this.cbMystery.Location = new System.Drawing.Point(111, 337);
+            this.cbMystery.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbMystery.Name = "cbMystery";
+            this.cbMystery.Size = new System.Drawing.Size(78, 17);
+            this.cbMystery.TabIndex = 6;
+            this.cbMystery.Text = "Мистерија";
+            this.cbMystery.UseVisualStyleBackColor = true;
+            // 
+            // cbThriller
+            // 
+            this.cbThriller.AutoSize = true;
+            this.cbThriller.Location = new System.Drawing.Point(204, 337);
+            this.cbThriller.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbThriller.Name = "cbThriller";
+            this.cbThriller.Size = new System.Drawing.Size(63, 17);
+            this.cbThriller.TabIndex = 7;
+            this.cbThriller.Text = "Трилер";
+            this.cbThriller.UseVisualStyleBackColor = true;
+            // 
+            // cbFantasy
+            // 
+            this.cbFantasy.AutoSize = true;
+            this.cbFantasy.Location = new System.Drawing.Point(18, 366);
+            this.cbFantasy.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbFantasy.Name = "cbFantasy";
+            this.cbFantasy.Size = new System.Drawing.Size(80, 17);
+            this.cbFantasy.TabIndex = 8;
+            this.cbFantasy.Text = "Фантазија";
+            this.cbFantasy.UseVisualStyleBackColor = true;
+            // 
+            // cbDrama
+            // 
+            this.cbDrama.AutoSize = true;
+            this.cbDrama.Location = new System.Drawing.Point(111, 366);
+            this.cbDrama.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbDrama.Name = "cbDrama";
+            this.cbDrama.Size = new System.Drawing.Size(61, 17);
+            this.cbDrama.TabIndex = 9;
+            this.cbDrama.Text = "Драма";
+            this.cbDrama.UseVisualStyleBackColor = true;
+            // 
+            // cbRomance
+            // 
+            this.cbRomance.AutoSize = true;
+            this.cbRomance.Location = new System.Drawing.Point(204, 366);
+            this.cbRomance.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbRomance.Name = "cbRomance";
+            this.cbRomance.Size = new System.Drawing.Size(71, 17);
+            this.cbRomance.TabIndex = 10;
+            this.cbRomance.Text = "Романса";
+            this.cbRomance.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1191, 819);
+            this.ClientSize = new System.Drawing.Size(596, 426);
+            this.Controls.Add(this.cbRomance);
+            this.Controls.Add(this.cbDrama);
+            this.Controls.Add(this.cbFantasy);
+            this.Controls.Add(this.cbThriller);
+            this.Controls.Add(this.cbMystery);
+            this.Controls.Add(this.cbHorror);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnIzbrisi);
+            this.Controls.Add(this.btnDodadi);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lbKnigi);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Библиотека";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -182,19 +302,27 @@
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox lbKnigi;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tbPubHouse;
+        private System.Windows.Forms.TextBox tbPubYear;
+        private System.Windows.Forms.RichTextBox rtbSummary;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Button btnDodadi;
+        private System.Windows.Forms.Button btnIzbrisi;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbSurname;
+        private System.Windows.Forms.CheckBox cbHorror;
+        private System.Windows.Forms.CheckBox cbMystery;
+        private System.Windows.Forms.CheckBox cbThriller;
+        private System.Windows.Forms.CheckBox cbFantasy;
+        private System.Windows.Forms.CheckBox cbDrama;
+        private System.Windows.Forms.CheckBox cbRomance;
     }
 }
 
--- source/VPBiblioteka/Form1.cs
+++ source/VPBiblioteka/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using MySql.Data.MySqlClient;
 namespace VPBiblioteka
 {
     public partial class Form1 : Form
@@ -15,6 +15,36 @@ namespace VPBiblioteka
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+              string Select = "SELECT * FROM Books;";
+              string Connection = "SERVER=oriondev.me;" + "DATABASE=VPLibrary;"
+             + "UID=Athena;" + "PASSWORD=masterofpuppets;";
+              MySqlDataReader Reader;
+              MySqlConnection con = new MySqlConnection(Connection);
+          try
+          {
+              con.Open();
+              MySqlCommand cmd = new MySqlCommand(Select, con);
+              Reader = cmd.ExecuteReader();
+              while (Reader.Read())
+              {
+                  lbKnigi.Items.Add(Reader.GetString("Book_name"));
+              }
+
+          }
+            catch(MySqlException ex)
+          {
+              DialogResult.OK.ToString(ex.Message);
+          }
+          finally
+          {
+              con.Close();
+          }
+
+
         }
     }
 }
