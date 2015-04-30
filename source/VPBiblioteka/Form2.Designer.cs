f4f7d82b4365da42a84c2e827810f17c97406924
Filip <boev.filip@students.finki.ukim.mk>
2015-04-30 02:44:09 +0200

    Validation support
    Added validation support for the needed fields. All that's left is to
    actually handle the validated data going in the DB. Also need to tweak
    the validations to correspond properly to their respective fields.

--- source/VPBiblioteka/Form2.Designer.cs
+++ source/VPBiblioteka/Form2.Designer.cs
@@ -28,37 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.components = new System.ComponentModel.Container();
+            this.tbIme = new System.Windows.Forms.TextBox();
+            this.tbImeKniga = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.tbPrezime = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.rtbSodrzina = new System.Windows.Forms.RichTextBox();
+            this.btnDodadi = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
             this.cbGenre = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.tbIzdavackaKukja = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbGodIzdavanje = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // tbIme
             // 
-            this.textBox1.Location = new System.Drawing.Point(24, 38);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(142, 20);
-            this.textBox1.TabIndex = 0;
+            this.tbIme.Location = new System.Drawing.Point(24, 38);
+            this.tbIme.Margin = new System.Windows.Forms.Padding(2);
+            this.tbIme.Name = "tbIme";
+            this.tbIme.Size = new System.Drawing.Size(142, 20);
+            this.tbIme.TabIndex = 0;
+            this.tbIme.Validating += new System.ComponentModel.CancelEventHandler(this.tbIme_Validating);
             // 
-            // textBox2
+            // tbImeKniga
             // 
-            this.textBox2.Location = new System.Drawing.Point(24, 87);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(142, 20);
-            this.textBox2.TabIndex = 1;
+            this.tbImeKniga.Location = new System.Drawing.Point(24, 87);
+            this.tbImeKniga.Margin = new System.Windows.Forms.Padding(2);
+            this.tbImeKniga.Name = "tbImeKniga";
+            this.tbImeKniga.Size = new System.Drawing.Size(142, 20);
+            this.tbImeKniga.TabIndex = 1;
+            this.tbImeKniga.Validating += new System.ComponentModel.CancelEventHandler(this.tbImeKniga_Validating);
             // 
             // label1
             // 
@@ -82,13 +89,14 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "Име на книгата";
             // 
-            // textBox3
+            // tbPrezime
             // 
-            this.textBox3.Location = new System.Drawing.Point(176, 38);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(142, 20);
-            this.textBox3.TabIndex = 4;
+            this.tbPrezime.Location = new System.Drawing.Point(176, 38);
+            this.tbPrezime.Margin = new System.Windows.Forms.Padding(2);
+            this.tbPrezime.Name = "tbPrezime";
+            this.tbPrezime.Size = new System.Drawing.Size(142, 20);
+            this.tbPrezime.TabIndex = 4;
+            this.tbPrezime.Validating += new System.ComponentModel.CancelEventHandler(this.tbPrezime_Validating);
             // 
             // label3
             // 
@@ -100,14 +108,6 @@
             this.label3.Size = new System.Drawing.Size(59, 15);
             this.label3.TabIndex = 5;
             this.label3.Text = "Презиме";
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(176, 87);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(142, 20);
-            this.textBox4.TabIndex = 6;
             // 
             // label4
             // 
@@ -131,25 +131,26 @@
             this.label5.TabIndex = 8;
             this.label5.Text = "Кратка содржина";
             // 
-            // richTextBox1
+            // rtbSodrzina
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(24, 192);
-            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(294, 157);
-            this.richTextBox1.TabIndex = 9;
-            this.richTextBox1.Text = "";
+            this.rtbSodrzina.Location = new System.Drawing.Point(24, 192);
+            this.rtbSodrzina.Margin = new System.Windows.Forms.Padding(2);
+            this.rtbSodrzina.Name = "rtbSodrzina";
+            this.rtbSodrzina.Size = new System.Drawing.Size(294, 157);
+            this.rtbSodrzina.TabIndex = 9;
+            this.rtbSodrzina.Text = "";
             // 
-            // button1
+            // btnDodadi
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(24, 353);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(115, 37);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "Додади";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnDodadi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDodadi.Location = new System.Drawing.Point(24, 353);
+            this.btnDodadi.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDodadi.Name = "btnDodadi";
+            this.btnDodadi.Size = new System.Drawing.Size(115, 37);
+            this.btnDodadi.TabIndex = 10;
+            this.btnDodadi.Text = "Додади";
+            this.btnDodadi.UseVisualStyleBackColor = true;
+            this.btnDodadi.Click += new System.EventHandler(this.btnDodadi_Click);
             // 
             // btnExit
             // 
@@ -170,6 +171,7 @@
             this.cbGenre.Name = "cbGenre";
             this.cbGenre.Size = new System.Drawing.Size(139, 21);
             this.cbGenre.TabIndex = 12;
+            this.cbGenre.Validating += new System.ComponentModel.CancelEventHandler(this.cbGenre_Validating);
             // 
             // label6
             // 
@@ -180,29 +182,65 @@
             this.label6.TabIndex = 13;
             this.label6.Text = "Жанр";
             // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // tbIzdavackaKukja
+            // 
+            this.tbIzdavackaKukja.Location = new System.Drawing.Point(24, 138);
+            this.tbIzdavackaKukja.Margin = new System.Windows.Forms.Padding(2);
+            this.tbIzdavackaKukja.Name = "tbIzdavackaKukja";
+            this.tbIzdavackaKukja.Size = new System.Drawing.Size(142, 20);
+            this.tbIzdavackaKukja.TabIndex = 14;
+            this.tbIzdavackaKukja.Validating += new System.ComponentModel.CancelEventHandler(this.tbIzdavackaKukja_Validating);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(24, 121);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(96, 15);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Издавачка куќа";
+            // 
+            // tbGodIzdavanje
+            // 
+            this.tbGodIzdavanje.Location = new System.Drawing.Point(176, 87);
+            this.tbGodIzdavanje.Margin = new System.Windows.Forms.Padding(2);
+            this.tbGodIzdavanje.Name = "tbGodIzdavanje";
+            this.tbGodIzdavanje.Size = new System.Drawing.Size(142, 20);
+            this.tbGodIzdavanje.TabIndex = 6;
+            this.tbGodIzdavanje.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbGodIzdavanje_KeyPress);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(338, 413);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tbIzdavackaKukja);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.cbGenre);
             this.Controls.Add(this.btnExit);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnDodadi);
+            this.Controls.Add(this.rtbSodrzina);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.tbGodIzdavanje);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.tbPrezime);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbImeKniga);
+            this.Controls.Add(this.tbIme);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form2";
             this.Text = "Додади Книга";
             this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -210,19 +248,22 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox tbIme;
+        private System.Windows.Forms.TextBox tbImeKniga;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox tbPrezime;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox rtbSodrzina;
+        private System.Windows.Forms.Button btnDodadi;
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.ComboBox cbGenre;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbIzdavackaKukja;
+        private System.Windows.Forms.TextBox tbGodIzdavanje;
     }
 }
--- source/VPBiblioteka/Form2.cs
+++ source/VPBiblioteka/Form2.cs
@@ -32,5 +32,121 @@ namespace VPBiblioteka
         {
             this.Close();
         }
+
+        private void tbIme_Validating(object sender, CancelEventArgs e)
+        {
+            if (tbIme.Text.Trim().Length == 0)
+            {
+                errorProvider1.SetError(tbIme, "Задолжително внеси име на автор.");
+                e.Cancel = false;
+            }
+            else
+            {
+                errorProvider1.SetError(tbIme, null);
+                e.Cancel = false;
+            }
+        }
+
+        private void tbPrezime_Validating(object sender, CancelEventArgs e)
+        {
+            if (tbPrezime.Text.Trim().Length == 0)
+            {
+                errorProvider1.SetError(tbPrezime, "Задолжително внеси презиме на автор.");
+                e.Cancel = false;
+            }
+            else
+            {
+                errorProvider1.SetError(tbPrezime, null);
+                e.Cancel = false;
+            }
+
+        }
+
+        private void tbImeKniga_Validating(object sender, CancelEventArgs e)
+        {
+            if (tbImeKniga.Text.Trim().Length == 0)
+            {
+                errorProvider1.SetError(tbImeKniga, "Задолжително внеси име на книга.");
+                e.Cancel = false;
+            }
+            else
+            {
+                errorProvider1.SetError(tbImeKniga, null);
+                e.Cancel = false;
+            }
+
+        }
+
+        private void tbIzdavackaKukja_Validating(object sender, CancelEventArgs e)
+        {
+            if (tbIzdavackaKukja.Text.Trim().Length == 0)
+            {
+                errorProvider1.SetError(tbIzdavackaKukja, "Задолжително внеси издавачка куќа.");
+                e.Cancel = false;
+            }
+            else
+            {
+                errorProvider1.SetError(tbIzdavackaKukja, null);
+                e.Cancel = false;
+            }
+        }
+
+        private void cbGenre_Validating(object sender, CancelEventArgs e)
+        {
+            if (cbGenre.SelectedIndex == -1)
+            {
+                MessageBox.Show("Изберете соодветен жанр!", "Несоодветен влез!");
+                e.Cancel = false;
+            }
+
+            else
+            {
+                errorProvider1.SetError(cbGenre, null);
+                e.Cancel = false;
+            }
+        }
+
+        private void tbGodIzdavanje_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= '0' && e.KeyChar <= '9' || e.KeyChar == '\b') //Backspace, smee da se brise
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+                MessageBox.Show("Внесете валидни години, само бројки.", "Несоодветен влез!");
+            }
+            if (tbGodIzdavanje.Text.Trim().Length > 3)
+            {
+                MessageBox.Show("Внесете валидна големина на години, 4 бројки.", "Несоодветен влез!");
+                tbGodIzdavanje.Text = "";
+                e.Handled = true;
+            }
+        }
+
+        private void btnDodadi_Click(object sender, EventArgs e)
+        {
+
+            if (
+                string.IsNullOrWhiteSpace(tbIme.Text)
+                ||
+                string.IsNullOrWhiteSpace(tbImeKniga.Text)
+                ||
+                string.IsNullOrWhiteSpace(tbPrezime.Text)
+                ||
+                string.IsNullOrWhiteSpace(tbIzdavackaKukja.Text)
+                ||
+                string.IsNullOrWhiteSpace(tbGodIzdavanje.Text)
+                ||
+                string.IsNullOrWhiteSpace(rtbSodrzina.Text)
+               )
+            {
+                MessageBox.Show("Сите полиња се задолжителни и мора да бидат пополнети за да се додаде книга!", "Недосататок на податоци!");
+                return;
+            }
+            //TODO: Change this dumb thing and actually add support for a real insert.
+            MessageBox.Show("Книгата е успешно додадена!", "Успех!");
+        }
     }
 }
